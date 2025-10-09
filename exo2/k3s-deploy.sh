0dcb84aab1db02b06320ad1d894b0599e4b199d6
divergnight <alsinus.divergnight@gmail.com>
2025-10-09 22:51:08 +0200

    Changes: Refactor Vagrant exo2

--- exo2/k3s-deploy.sh
+++ exo2/k3s-deploy.sh
@@ -1,46 +1,7 @@
 #!/bin/bash
 
-for i in {1..3}; do
-    name="app$i"
-    kubectl create deployment $name --image=gcr.io/google-samples/hello-app:1.0
-    kubectl expose deployment $name --port=80 --target-port=8080
-done
+mkdir -p /opt/manifests
 
-cat <<'EOF' | kubectl apply -f -
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: hello-ingress
-spec:
-  rules:
-  - host: app1
-    http:
-      paths:
-      - path: /
-        pathType: Prefix
-        backend:
-          service:
-            name: app1
-            port:
-              number: 80
-  - host: app2
-    http:
-      paths:
-      - path: /
-        pathType: Prefix
-        backend:
-          service:
-            name: app2
-            port:
-              number: 80
-  - host: app3
-    http:
-      paths:
-      - path: /
-        pathType: Prefix
-        backend:
-          service:
-            name: app3
-            port:
-              number: 80
-EOF
+cp -r /mnt/* /opt/manifests
+
+kubectl apply -f /opt/manifests --recursive
