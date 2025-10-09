36b5edaa9ac6ea02e6e5e4e97877c03ca2be96b0
divergnight <alsinus.divergnight@gmail.com>
2025-10-09 12:49:23 +0200

    Fix: Remove useless code

--- exo1/k3s-agent-install.sh
+++ exo1/k3s-agent-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for i in $(seq 1 30); do
-  if [ -f /mnt/k3s_token ] && nc -z 192.168.122.12 6443; then break; fi
+  if [ -f /mnt/k3s_token ] && nc -z 192.168.122.110 6443; then break; fi
   sleep 2
 done
 
-K3S_URL="https://192.168.122.12:6443/"
+K3S_URL="https://192.168.122.110:6443/"
 K3S_TOKEN=$(cat /mnt/k3s_token)
 
 curl -sfL https://get.k3s.io/ | K3S_URL=$K3S_URL K3S_TOKEN=$K3S_TOKEN sh -
